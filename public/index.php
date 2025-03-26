b3b2204371b86f82d8bb9d80339c99cb702fe76c
giardm <maxence.giard@outlook.com>
2025-03-26 12:44:17 +0100

    import font + style header

--- public/index.php
+++ public/index.php
@@ -5,14 +5,34 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <title>Document</title>
 </head>
 
 <body>
-  <video autoplay loop muted playsinline>
+  <header>
+    <div class="logo">
+      <img id="mobileLogo" src="./images/logo-03-b.png" alt="">
+      <img id="desktopLogo" src="./images/logo-04-b.png" alt="">
+    </div>
+    <div class="searchContainer">
+      <div class="searchInput">
+        <i class="fa-solid fa-magnifying-glass"></i>
+        <input type="text" placeholder="Recherchez un jeu au meilleur prix...">
+      </div>
+      <button class="searchButton">
+        <i class="fa-solid fa-magnifying-glass"></i>
+      </button>
+    </div>
+    <div class="profil">
+      <i class="fa-regular fa-user"></i>
+      <i class="fa-light fa-user"></i>
+    </div>
+  </header>
+  <!-- <video autoplay loop muted playsinline>
     <source src="../public/videos/logo.webm" type="video/webm">
     <source src="../public/videos/logo.mp4" type="video/mp4">
-  </video>
+  </video> -->
 </body>
 
 </html>
