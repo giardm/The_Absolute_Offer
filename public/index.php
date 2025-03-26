cbb4ef52c09acd0857e93e6a805b940df18943b9
giardm <maxence.giard@outlook.com>
2025-03-26 10:13:15 +0100

    ajout logos et animation

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <video autoplay loop muted playsinline>
+    <source src="../public/videos/logo.webm" type="video/webm">
+    <source src="../public/videos/logo.mp4" type="video/mp4">
+  </video>
+</body>
+
+</html>
