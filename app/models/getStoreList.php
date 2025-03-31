d348b3e79be48ed391e2da517adc41e445d8c0a3
giardm <maxence.giard@outlook.com>
2025-03-31 16:31:45 +0200

    ajout liste des offres

---
+++ app/models/getStoreList.php
@@ -0,0 +1,11 @@
+<?php
+$storesJson = file_get_contents("https://www.cheapshark.com/api/1.0/stores");
+$stores = json_decode($storesJson, true);
+$storeList = [];
+
+foreach ($stores as $store) {
+  $storeList[$store['storeID']] = [
+    'name' => $store['storeName'],
+    'logo' => 'https://www.cheapshark.com' . $store['images']['logo'],
+  ];
+}
--- app/views/productView.php
+++ app/views/productView.php
@@ -1,5 +1,6 @@
 <?php
 require_once VIEWS_PATH . ('/productView.php');
+require_once MODELS_PATH . ('/getStoreList.php');
 
 $steamAppID = $gameData['info']['steamAppID'] ?? null;
 $cheapsharkTitle = $gameData['info']['title'] ?? 'Titre non disponible';
@@ -19,62 +20,104 @@ $afficheUrl = $steamAppID
     </div>
   </div>
 
-  <ul>
-    <li><strong>Meilleur prix :</strong> <?= $cheapsharkPrice ?> €</li>
-
-    <?php if (!empty($steamData)): ?>
-      <li><strong>Date de sortie :</strong> <?= $steamData['release_date']['date'] ?? '' ?></li>
-      <li><strong>Éditeur(s) :</strong> <?= implode(', ', $steamData['publishers']) ?></li>
-      <li><strong>Genres :</strong>
-        <?= implode(', ', array_map(fn($g) => $g['description'], $steamData['genres'])) ?>
-      </li>
-      <?php if (!empty($steamData['metacritic'])): ?>
-        <li><strong>Score Metacritic :</strong> <?= $steamData['metacritic']['score'] ?>/100</li>
-      <?php endif; ?>
-      <?php if (!empty($steamData['recommendations'])): ?>
-        <li><strong>Évaluations Steam :</strong> <?= number_format($steamData['recommendations']['total']) ?> avis</li>
-      <?php endif; ?>
-    <?php endif; ?>
-  </ul>
-
-  <?php if (!empty($steamData['about_the_game'])): ?>
-    <h3>Informations</h3>
-    <div class="description">
-      <?= $steamData['about_the_game'] ?>
-    </div>
-  <?php endif; ?>
-
-  <div class="medias">
-    <?php if (!empty($steamData['movies'])): ?>
-      <div class="videos">
-        <h2>Bande-annonce</h2>
-        <video controls>
-          <?php
-          $videoUrl = $steamData['movies'][0]['webm']['max'];
-          $secureVideoUrl = preg_replace('/^http:/', 'https:', $videoUrl);
-          ?>
-          <source src="<?= $secureVideoUrl ?>" type="video/webm">
-        </video>
+  <div class="productMain">
+    <div class="productInformations">
+      <div class="informations">
+        <ul>
+          <h3>Informations</h3>
+          <li><strong>Meilleur prix jamais enregistré :</strong> <?= $cheapsharkPrice ?> €</li>
+          <?php if (!empty($steamData)): ?>
+            <li><strong>Date de sortie :</strong> <?= $steamData['release_date']['date'] ?? '' ?></li>
+            <li><strong>Éditeur(s) :</strong> <?= implode(', ', $steamData['publishers']) ?></li>
+            <li><strong>Genres :</strong>
+              <?= implode(', ', array_map(fn($g) => $g['description'], $steamData['genres'])) ?>
+            </li>
+            <?php if (!empty($steamData['metacritic'])): ?>
+              <li><strong>Score Metacritic :</strong> <?= $steamData['metacritic']['score'] ?>/100</li>
+            <?php endif; ?>
+            <?php if (!empty($steamData['recommendations'])): ?>
+              <li><strong>Évaluations Steam :</strong> <?= number_format($steamData['recommendations']['total']) ?> avis</li>
+            <?php endif; ?>
+          <?php endif; ?>
+        </ul>
       </div>
-    <?php endif; ?>
-
-    <?php if (!empty($steamData['screenshots'])): ?>
-      <div class="screenshots">
-        <h2>Captures d'écran</h2>
-        <div class="screenshotsGallery">
-          <?php foreach ($steamData['screenshots'] as $index => $shot): ?>
-            <img
-              src="<?= $shot['path_thumbnail'] ?>"
-              data-full="<?= $shot['path_full'] ?>"
-              data-index="<?= $index ?>"
-              class="screenshotThumb"
-              alt="Screenshot">
+      <div class="offers">
+        <h3>Offres disponibles</h3>
+        <ul class="offerList">
+          <?php foreach ($gameData['deals'] as $deal): ?>
+            <?php
+            // $storeID = $deal['storeID'];
+            $storeID = (string)$deal['storeID'];
+            echo "<!-- Store ID: $storeID -->"; // Voir dans le code source HTML
+            $store = $storeMap[$storeID] ?? ['name' => 'Store inconnu', 'logo' => ''];
+            $savings = round($deal['savings']);
+            $dealUrl = "https://www.cheapshark.com/redirect?dealID={$deal['dealID']}";
+            ?>
+            <li class="offerItem">
+              <a class="offerCard" href="<?= $dealUrl ?>" target="_blank">
+                <?php if ($store['logo']): ?>
+                  <img class="storeLogo" src="<?= $store['logo'] ?>" alt="<?= $store['name'] ?>">
+                <?php endif; ?>
+                <div class="offerInfo">
+                  <p class="storeName"><?= $store['name'] ?></p>
+                  <p class="prices">
+                    <span class="price"><?= $deal['price'] ?> €</span>
+                    <span class="retailPrice"><?= $deal['retailPrice'] ?> €</span>
+                  </p>
+                  <span class="discount">-<?= $savings ?>%</span>
+                </div>
+              </a>
+            </li>
           <?php endforeach; ?>
-        </div>
+        </ul>
+
+      </div>
+    </div>
+
+    <?php if (!empty($steamData['about_the_game'])): ?>
+      <h3>Description</h3>
+      <div class="description">
+        <?= $steamData['about_the_game'] ?>
       </div>
     <?php endif; ?>
+
+    <div class="medias">
+      <?php if (!empty($steamData['movies'])): ?>
+        <div class="videos">
+          <h2>Bande-annonce</h2>
+          <video controls>
+            <?php
+            $videoUrl = $steamData['movies'][0]['webm']['max'];
+            $secureVideoUrl = preg_replace('/^http:/', 'https:', $videoUrl);
+            ?>
+            <source src="<?= $secureVideoUrl ?>" type="video/webm">
+          </video>
+        </div>
+      <?php endif; ?>
+
+      <?php if (!empty($steamData['screenshots'])): ?>
+        <div class="screenshots">
+          <h2>Captures d'écran</h2>
+          <div class="screenshotsGallery">
+            <?php foreach ($steamData['screenshots'] as $index => $shot): ?>
+              <img
+                src="<?= $shot['path_thumbnail'] ?>"
+                data-full="<?= $shot['path_full'] ?>"
+                data-index="<?= $index ?>"
+                class="screenshotThumb"
+                alt="Screenshot">
+            <?php endforeach; ?>
+          </div>
+        </div>
+      <?php endif; ?>
+    </div>
   </div>
+
+
+
 </div>
+
+
 
 
 
