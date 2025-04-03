82fd3ec6c61027df1a20073c6136b06493d390cf
giardm <maxence.giard@outlook.com>
2025-04-03 09:24:27 +0200

    index a la racine

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
+
+require(__DIR__ . '/config/config.php');
+
+if (isset($_GET["action"])) {
+  $action = $_GET["action"];
+} else {
+  $action = "home";
+}
+
+
+require CONTROLLERS_PATH . "/router.php";
