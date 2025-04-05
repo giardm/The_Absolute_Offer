1d668261d1c01cbcd92500e60d7d32e207575576
Maxence <maxence.giard@outlook.com>
2025-04-05 18:32:11 +0200

    update fonction

--- app/models/connexionDb.php
+++ app/models/connexionDb.php
@@ -4,14 +4,14 @@ require_once BASE_PATH . '/config/config.php';
 
 function connexionPDO()
 {
-  try {
-    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8", DB_USER, DB_PASS, [
+  return new PDO(
+    "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8",
+    DB_USER,
+    DB_PASS,
+    [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    ]);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $pdo;
-  } catch (PDOException $e) {
-    die("Erreur de connexion PDO ");
-  }
+    ]
+  );
 }
+
