96d61a5c781cee46476d960f456ca453708406c3
giardm <maxence.giard@outlook.com>
2025-04-09 09:23:24 +0200

    redirection apres deconnexion

--- app/controllers/users/logoutController.php
+++ app/controllers/users/logoutController.php
@@ -3,6 +3,4 @@ require_once MODELS_PATH . '/userModel.php';
 
 logout();
 
-require_once VIEWS_PATH . '/partials/headerView.php';
-require_once VIEWS_PATH . '/homeView.php';
-require_once VIEWS_PATH . '/partials/footerView.php';
+require_once CONTROLLERS_PATH . '/home/homeController.php';
