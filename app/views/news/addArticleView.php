9d1963fb897991614ea744fd8401f67f3fb2c9b9
giardm <maxence.giard@outlook.com>
2025-04-18 15:24:29 +0200

    style page ajout d'article

--- app/views/news/addArticleView.php
+++ app/views/news/addArticleView.php
@@ -1,28 +1,28 @@
 <?php require VIEWS_PATH . "/partials/headerView.php"; ?>
-<div class="conainer">
-  <form id="articleForm" action="save_article.php" method="post" enctype="multipart/form-data">
-    <label for="titleInput">Titre:</label><br>
-    <input type="text" name="title" id="titleInput" required><br><br>
 
-    <label for="contentEditor">Contenu:</label><br>
-    <textarea name="content" id="contentEditor" rows="10" required></textarea><br><br>
+<form id="articleForm" action="save_article.php" method="post" enctype="multipart/form-data">
+  <h2>Ajouter un article</h2>
+  <label for="titleInput">Titre:</label>
+  <input type="text" name="title" id="titleInput">
 
-    <label for="categorySelect">Categorie:</label><br>
-    <select name="category" id="categorySelect" required>
-      <?php foreach ($categories as $cat): ?>
-        <option value="<?= $cat['category_id'] ?>"><?= htmlspecialchars($cat['name']) ?></option>
-      <?php endforeach; ?>
-    </select><br><br>
+  <label for="contentEditor">Contenu:</label>
+  <textarea name="content" id="contentEditor" rows="10"></textarea>
 
-    <label for="imageUpload">Image (format webp):</label><br>
-    <input type="file" name="image" id="imageUpload" accept=".webp" required><br><br>
+  <label for="categorySelect">Categorie:</label>
+  <select name="category" id="categorySelect">
+    <?php foreach ($categories as $cat): ?>
+      <option value="<?= $cat['category_id'] ?>"><?= htmlspecialchars($cat['name']) ?></option>
+    <?php endforeach; ?>
+  </select>
 
-    <label for="thumbAltInput">Description de l'image :</label><br>
-    <input type="text" name="thumbAlt" id="thumbAltInput" required><br><br>
+  <label for="imageUpload">Image (format webp):</label>
+  <input type="file" name="image" id="imageUpload" accept=".webp">
 
-    <button type="submit">Ajouter l'article</button>
-  </form>
-</div>
+  <label for="thumbAltInput">Description de l'image :</label>
+  <input type="text" name="thumbAlt" id="thumbAltInput">
+
+  <button id="addBtn" type="submit">Ajouter l'article</button>
+</form>
 
 <!-- jQuery + Trumbowyg -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
